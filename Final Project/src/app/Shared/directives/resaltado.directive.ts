8f6863dd4dbc55f78c87f8d722113709772971d1
Gothmaur <78580948+Gothmaur@users.noreply.github.com>
2023-07-18 23:07:37 -0600

    Se agrega funcionamiento de directiva

--- Final Project/src/app/Shared/directives/resaltado.directive.ts
+++ Final Project/src/app/Shared/directives/resaltado.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
-export class ResaltadoDirective {
+export class ResaltadoDirective implements OnChanges {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer2: Renderer2) { 
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.cambiarFontSize();
+  }
 
+  cambiarFontSize(){
+    this.renderer2.setStyle(
+      this.elementRef.nativeElement,
+      'font-size',
+      20
+    );this.renderer2.setStyle(
+      this.elementRef.nativeElement,
+      'color',
+      'red'
+    );
+
+  }
 }
