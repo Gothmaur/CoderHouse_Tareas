5800f4358daa9d77abffe4f57c97575f79c313f2
Gothmaur <78580948+Gothmaur@users.noreply.github.com>
2023-09-03 20:32:39 -0600

    Revert "Modulo de Productos (Cursos)"
    This reverts commit 4194f8916f6ce1ed2f60c01f2784a0a1db42da2a.

--- Final Project/src/app/dashboard/pages/productos/productos.module.ts
+++ Final Project/src/app/dashboard/pages/productos/productos.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductosComponent } from './productos.component';
-import { SharedModule } from 'src/app/Shared/shared.module';
-import { ProductDialogComponent } from './components/product-dialog/product-dialog.component';
-import { ProductTableComponent } from './components/product-table/product-table.component';
-import { RouterModule } from '@angular/router';
-import { ProductsRoutingModule } from './productos-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    ProductosComponent,
-    ProductDialogComponent,
-    ProductTableComponent
+    ProductosComponent
   ],
   imports: [
-    RouterModule,
-    CommonModule,
-    SharedModule,
-    ProductsRoutingModule
+    CommonModule
   ]
 })
 export class ProductosModule { }
